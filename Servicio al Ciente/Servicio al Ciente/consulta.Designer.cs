5b8d476ada39672896dd0c5a970eb4b1047cd8d0
cesar <cesaroliva2009@hotmail.com>
2013-08-13 19:49:11 -0600

    p
    p

--- Servicio al Ciente/Servicio al Ciente/consulta.Designer.cs
+++ Servicio al Ciente/Servicio al Ciente/consulta.Designer.cs
@@ -34,6 +34,7 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -83,11 +84,21 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 89);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "125";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // consulta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(868, 306);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
@@ -108,5 +119,6 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
